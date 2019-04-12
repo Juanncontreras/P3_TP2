f8a589188f1969f130c773e9266e999f82232a5f
Juan Contreras <Juanncontreras@gmail.com>
2019-04-11 23:42:41 -0300

    first commit

---
+++ Dominio/Dominio/Depositos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Depositos
+    {
+        public Pasillos pasillos;
+        public Compartimientos compartimientos;
+        public Empleados empleados;
+        public Productos productos;
+        public Clientes clientes;
+
+    }
+}
